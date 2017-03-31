79e5303bd933f20e3efe341262cb7dd6e67fc609
TeapoyYinYue <605528224@qq.com>
2017-03-31 21:04:01 +0800

    1

--- fume/models.py
+++ fume/models.py
@@ -4,46 +4,61 @@ from django.db import models
 
 # Create your models here.
 class Game(models.Model):
-    game = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    description = models.TextField()
+    game = models.CharField(max_length=200,blank=True, null=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2,blank=True, null=True)
+    game_id = models.CharField(max_length=200,blank=True, null=True)
+    def __str__(self):
+        return self.game
 
 
 class User(models.Model):
-    name = models.CharField(max_length=200)
-    userId = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    name = models.CharField(max_length=200,blank=True, null=True)
+    userId = models.CharField(max_length=200,blank=True, null=True)
+    email = models.CharField(max_length=200,blank=True, null=True)
     def __str__(self):
         return self.name    
 
 
 class Tag(models.Model):
-    tag = models.CharField(max_length=50)
-    creater = models.CharField(max_length=200)
+    tag = models.CharField(max_length=50,blank=True, null=True)
+    creator = models.CharField(max_length=200,blank=True, null=True)
+   # game = models.ForeignKey(Game,blank=True, null=True)
+
     def __str__(self):
         return self.tag
+
 
     
 class Tagging(models.Model):
     #userId = models.ForeignKey(User)
-    tag = models.ForeignKey(Tag)
+    tag = models.ForeignKey(Tag,blank=True, null=True)
     def __str__(self):
         return self.tag
     
 class Purchase(models.Model):
-    userId = models.ForeignKey(User)
-    game = models.ForeignKey(Game)
+    userId = models.ForeignKey(User,blank=True, null=True)
+    game = models.ForeignKey(Game,blank=True, null=True)
 
 class Cart(models.Model):
     user = models.ForeignKey(User)
-    game = models.ForeignKey(Game)
+    game = models.ManyToManyField(Game,blank=True, null=True)
+    def addGame(self,game):
+        self.game.add(game)
+    def getTotal(self):
+        games = self.game.all()
+        amount = self.game.all().count()
+        totalAmount = 0
+        for gme in games:
+            price = Game.objects.get(game=gme).price
+            totalAmount = totalAmount + price
+        return totalAmount
 
 class Reward(models.Model):
     timeReceived = models.DateTimeField(blank=True, null=True)
-    amount = models.
-    def receiveReward(self):
+    amount = models.CharField(max_length=200)
+    def receiveReward(self):                    # to record the time when the reward is received
             self.timeReceived = timezone.now()
             self.save()
 class Administrator(models.Model):
-    ID = models.CharField(max_length=200)
+    adminID = models.CharField(max_length=200)
 
