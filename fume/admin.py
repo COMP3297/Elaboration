4c54e71c1480f6f36a262e5ca54f67e1e2c9ea73
TeapoyYinYue <605528224@qq.com>
2017-03-31 21:03:30 +0800

    purchase and tag functional( no crush)
    created test user and cart for demonstration

--- fume/admin.py
+++ fume/admin.py
@@ -2,14 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Game
-from .models import User
-from .models import Tagging
-from .models import Tag
-from .models import Purchase
+from .models import Game , User, Tagging , Tag, Purchase, Cart
+
 
 admin.site.register(Game)
 admin.site.register(User)
 admin.site.register(Tagging)
 admin.site.register(Tag)
 admin.site.register(Purchase)
+admin.site.register(Cart)
--- fume/views.py
+++ fume/views.py
@@ -2,32 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from fume.models import Game,Cart,Tag
+from fume.models import Game,Cart,Tag,User
 from fume.forms import NameForm
 
-
+	
 def featured(request):
 	return render(request, 'fume/featured.html', {})
 
 def games(request, game_id):
-	#game = Game.objects.get(id=game_id)
+	game = Game.objects.get(game_id=game_id)
 	print(game_id)
-	addCart = Game.objects.filter(id=game_id)
+	addCart = Game.objects.get(game_id=game_id)
 	return render(request, 'fume/'+ game_id + '.html', {'tag_id':game_id})
 	
 def purchase(request, game_id):
-	addCart=Game.objects.filter(id=game_id)
-	addedCart=Cart(game=addCart[0])
-	addedCart.save()
-	games = Cart.objects.all()
-	amount = Cart.objects.all().count()
-	totalAmount = 0
-	for gme in games:
-		totalAmount = totalAmount + gme.game.price
+	print("purchasing")
+	newgame=Game.objects.get(game_id=game_id)
+	user = User.objects.get(userId='123')
+	this_cart = Cart.objects.get(user = user)
+	this_cart.addGame(newgame)
+	amount = this_cart.game.all().count()
+	games = this_cart.game.all()
+	totalAmount = this_cart.getTotal()
 	return render(request, 'fume/purchase.html', {'games': games, 'amount': amount, 'totalAmount': totalAmount})
 
 def purchaseAll(request):
-	Cart.objects.all().delete()
+	#Cart.objects.game.delete()
 	return render(request, 'fume/featured.html', {})
 
 def tagedit(request, game_id):
@@ -38,9 +38,8 @@ def tagedit(request, game_id):
 	
 
 def addtag(request, game_id):
-	print (1111)
+	print ("adding tag")
 # if this is a POST request we need to process the form data
-	print (request.method)
 	if request.method == 'POST':
 		# create a form instance and populate it with data from the request:
 
@@ -56,7 +55,7 @@ def addtag(request, game_id):
 		#creator = request.POST.get('creator','')
 		tag_obj = Tag(tag=tag,creator=creator)
 		tag_obj.save()
-		print (1111111111111111111)
+		print ("tag added")
 		# redirect to a new URL:
 		return render(request, 'fume/'+ game_id + '.html', {'tag_id':game_id})
 
