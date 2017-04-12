c01c815fb7c9b8f68e02501f5e12d4d0a191a382
GaiHiker <gaihiker@likelion.org>
2017-04-12 16:31:46 +0000

    interim error fixed

--- fume/views.py
+++ fume/views.py
@@ -3,15 +3,18 @@ from django.shortcuts import render,redirect
 # Create your views here.
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from fume.models import Game,Cart,Tag,User,Recommendation
+from fume.models import Game,Cart,Tag,User
 from fume.forms import NameForm
 from fume.forms import LoginForm
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from fume.forms import SignUpForm
 
 
+	
+def featured(request):
+	return render(request, 'fume/featured.html', {})
+	
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -29,10 +32,7 @@ def signup(request):
 def games(request, game_id):
 	game = Game.objects.get(game_id=game_id)
 	tags = Tag.objects.filter(game=game).all()
-	imageList = game.getImageList()
-	image1 = imageList[0]
-	image2 = imageList[1]
-	return render(request, 'fume/gamePage.html', {'tag_id':game_id, 'tags':tags,'game':game,'image1':image1,'image2':image2})
+	return render(request, 'fume/gamePage.html', {'tag_id':game_id, 'tags':tags})
 	
 def purchase(request, game_id):
 	print("purchasing")
@@ -59,7 +59,7 @@ def tagedit(request, game_id):
 
 def addtag(request, game_id):
 	print ("adding tag")
-	# if this is a POST request we need to process the form data
+# if this is a POST request we need to process the form data
 	if request.method == 'POST':
 		# create a form instance and populate it with data from the request:
 
@@ -80,36 +80,10 @@ def addtag(request, game_id):
 			
 			# redirect to a new URL:
 		return redirect('games',game_id=game_id)
+
+
 	# if a GET (or any other method) we'll create a blank form
 	else:
 		return redirect('games',game_id=game_id)
 
-
-def getRcmdList(tagList):
-	rcmdList = []
-	for eachTag in tagList:
-		# check if the game is in the rcmdList already
-		if rcmdList.get(eachTag.game, "F") != "F":
-			rcmdList.append(eachTag.game)
-		if len(rcmdList) >= 4:
-			break
-	return rcmdList
 	
-def featured(request):
-	# Get a list of tags from user purchase history
-	
-	# Look for games that are affliated with the tags
-	rcmdList = []
-	for eachTag in tagList:
-		# check if the game is in the rcmdList already
-		if rcmdList.get(eachTag.game, "F") != "F":
-			rcmdList.append(eachTag.game)
-		if len(rcmdList) >= 4:
-			break
-
-	return render(request, 'fume/featured.html', rcmdList)
-
-
-
-
-
